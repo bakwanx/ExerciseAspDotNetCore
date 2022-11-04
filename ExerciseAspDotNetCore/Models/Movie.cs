09ccfe3fb57f263cc0a68b3610fc5f77ead58858
farhan <farhanfadhilah87@gmail.com>
2022-11-04 22:29:41 +0700

    Add project files.

---
+++ ExerciseAspDotNetCore/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciseAspDotNetCore.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public String? Title { get; set; }
+
+        public String? Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
